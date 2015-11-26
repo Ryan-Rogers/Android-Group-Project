1d535030ab405eae717efd85c77209c0a6139383
Ryan Rogers <ryan.rogers@mavs.uta.edu>
2015-11-26 21:40:05 +0000

    Background changes on navigation

--- navigation.js
+++ navigation.js
@@ -24,12 +24,21 @@ var display = function(contentId) {
         // Updating currently shown content
         hide(currentContent);
         currentContent = contentId;
+        changeBackground(contentId);
         
         // Showing input content
         document.getElementById(contentId).style.visibility = "visible";
         document.getElementById(contentId).style.display = "table";
     }
-}
+};
+
+// Changes background of body to input content
+var changeBackground = function(contentId) {
+    var body = document.getElementsByTagName("BODY")[0];
+    body.style.backgroundImage = "url(" + contentId + "_bg.jpg)";
+    body.style.backgroundRepeat = "repeat-y";
+    body.style.backgroundSize = "100%";
+};
 
 // At load time
 var landingPage = function() {
