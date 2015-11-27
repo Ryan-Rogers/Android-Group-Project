24767e68dc7260d545072840589398f1039a73fa
Ryan Rogers <ryan.rogers@mavs.uta.edu>
2015-11-27 04:18:39 +0000

    Search implemented

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var listings = [];
 var user = "anon";
 
 // Functions
+// Create a new listing
 createListing = function() {
+    
+    // Creating a new listing
     var newListing = {name: "", cost: "", quantity: "", deliveryType: "", 
             appointments: "", venues: "", user: ""};
     newListing.name = document.getElementById("newListingName").value;
@@ -15,30 +18,42 @@ createListing = function() {
     newListing.appointments = document.getElementById("newListingAppointments").value;
     newListing.venues = document.getElementById("newListingVenues").value;
     newListing.user = user;
+    
+    // Adding to listings[]
     listings.push(newListing);
     console.log(newListing.name + " created");
+    
+    // Updating Home
     updateHome();
 };
 
+// Update Home
 updateHome = function() {
     
-    // Filtering with Search field
-    var search = document.getElementById("search").value;
+    // Unpopulating Home
+    var listing;
+    for(listing = 0; listing < 6; listing++) {
+        document.getElementById("item" + listing).style.background = "rgba(63, 186, 255, 0.0)";
+        document.getElementById("item" + listing + "_name").innerHTML = "";
+        document.getElementById("item" + listing + "_price").innerHTML = "";
+    }
+    
+    // Filtering with Search text
+    var search = document.getElementById("search").value.toLowerCase();
     var filteredListings = [];
     listings.forEach(function(item) {
-        if(item.name.indexOf(search) !== -1) filteredListings.push(item);
+        if(item.name.toLowerCase().indexOf(search) !== -1) filteredListings.push(item);
     })
     
-    var listing;
-    // Populating Home
+    // Repopulating Home
     for(listing = 0; listing < 6 && listing < filteredListings.length; listing++) {
         document.getElementById("item" + listing).style.background = "rgba(63, 186, 255, 0.8)";
         document.getElementById("item" + listing + "_name").innerHTML = filteredListings[listing].name;
         document.getElementById("item" + listing + "_price").innerHTML = filteredListings[listing].cost;
     }
-    console.log("test");
 };
 
+// At load time
 window.onload = function() {
     updateHome();
 };
